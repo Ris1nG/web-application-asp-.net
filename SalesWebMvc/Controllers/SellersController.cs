0b0b6f74628726b7c5aac84e332d8b27fc0f78c9
Ris1nG <johndodo07@gmail.com>
2021-02-02 22:43:11 -0300

    Created a simple form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 using System;
 using System.Collections.Generic;
@@ -17,6 +18,15 @@ namespace SalesWebMvc.Controllers {
         public IActionResult Index() {
             // var list = _sellerService.FindAll();
             return View(_sellerService.FindAll());
+        }
+        public IActionResult Create() {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller) {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
